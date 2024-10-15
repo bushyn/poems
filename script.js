c9f9620c0879bb1e62dde688b64ff61dc64f64c3
Arseniy Bushyn <arseniybushyn@gmail.com>
2024-10-15 07:04:43 +0000

    After all this time?

--- script.js
+++ script.js
@@ -3,72 +3,51 @@
 
 let fs = require('fs');
 let path = require('path');
-let jade = require('jade');
-let CleanCSS = require('clean-css');
-let pretty = false;
+let pug = require('pug');
+let cleanCSS = require('clean-css');
 
-function partition(str, sep) {
-    let index = str.indexOf(sep);
-    if (index == -1) {
-      return  {left: str, right: ''}
-    } else {
-      return  {left: str.slice(0, index),
-              right: str.slice(index + sep.length)}
-    }
+const poems = fs.readdirSync('poems').map(fn => path.join('poems', fn)).map(readPoem);
+
+minifyCss();
+writePoems(poems);
+writeIndex(poems);
+
+function readPoem(fileName) {
+  const content = fs.readFileSync(fileName, 'utf-8');
+  const [header, text] = splitHeader(content);
+  const [title, author] = splitAuthor(header);
+  return { id: header.replace(/(\s)+/g, '-'), title, author, text};
 }
 
+function splitHeader(s) {
+  const i = s.indexOf("\n\n");
+  return [s.slice(0, i), s.slice(i + 2)];
+}
 
-function readPoems(files) {
-  function readPoem(file) {
-    let content = fs.readFileSync(file, 'utf-8');
-    let parts = partition(content, '\n\n');
-    let header = parts.left,
-        text = parts.right;
-    parts = partition(header, '\n');
-    let title = parts.left,
-        author = parts.right;
-    return {id: (header).replace(/(\s|\.|\,)+/g, '-'),
-              title: title, author: author.trim(), text: text};
-  }
-  return files.map(readPoem);
+function splitAuthor(s) {
+  const i = s.lastIndexOf("\nby");
+  return i > 0 ? [s.slice(0, i), s.slice(i + 1)] : [s, undefined];
 }
 
 function writePoems(poems) {
-  let poemHTML = jade.compileFile('./poem.jade', {pretty: pretty});
-
-  function writePoem(poem) {
-    try {
-      fs.mkdirSync(poem.id);
-    } catch (err) {
-      if (err.code != 'EEXIST') throw err;
-    }
-    fs.writeFileSync(poem.id + '/index.html', poemHTML(poem));
-  }
-
-  for(let i = 0, l = poems.length; i < l; i++) {
-    let poem = poems[i];
-    poem.prev = i? poems[i-1].id : poems[l-1].id;
-    poem.next = (i==l-1)? poems[0].id : poems[i+1].id;
-    writePoem(poem);
+  const poemToHTML = pug.compileFile('poem.pug');
+  for (let i = 0, l = poems.length; i < l; i++) {
+    const poem = poems[i];
+    poem.prev = poems.at(i - 1).id;
+    poem.next = poems.at((i + 1) % l).id;
+    fs.mkdirSync(poem.id, { recursive: true });
+    fs.writeFileSync(path.join(poem.id, 'index.html'), poemToHTML(poem));
   }
 }
 
 function writeIndex(poems) {
-  let indexHTML = jade.compileFile('./index.jade', {pretty: pretty});
-  fs.writeFileSync('./index.html', indexHTML({poems:poems}));
+  fs.writeFileSync('index.html', pug.renderFile('index.pug', { poems }));
 }
 
 function minifyCss() {
   let styles = fs.readFileSync('./styles.css');
-  fs.writeFileSync('./styles.min.css', (new CleanCSS()).minify(styles).styles);
+  fs.writeFileSync('./styles.min.css', (new cleanCSS()).minify(styles).styles);
 }
-
-if (!pretty) minifyCss();
-
-let files = fs.readdirSync('./poems').map(f => path.join('./poems', f));
-let poems = readPoems(files);
-writePoems(poems);
-writeIndex(poems);
 
 fs.watch('./', (event, filename) => {
   console.log((new Date).toLocaleTimeString(), event, filename);
@@ -76,14 +55,14 @@ fs.watch('./', (event, filename) => {
     try {
       switch (filename) {
         case 'styles.css':
-          if (!pretty) minifyCss();
+          minifyCss();
           writePoems(poems);
           writeIndex(poems);
           break;
-        case 'index.jade':
+        case 'index.pug':
           writeIndex(poems);
           break;
-        case 'poem.jade':
+        case 'poem.pug':
           writePoems(poems);
           break;
       }
