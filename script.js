76667356f9ff09b618a10eabad77ac6977a53bf9
Arseniy Bushyn <arseniybushyn@gmail.com>
2024-10-17 20:31:56 +0000

    add prefetch

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
 #!/usr/bin/env node
 'use strict';
 
-let fs = require('fs');
-let path = require('path');
-let pug = require('pug');
-let cleanCSS = require('clean-css');
+import { readFileSync as fread, writeFileSync as fwrite, readdirSync as readdir, mkdirSync as mkdir, watch } from 'fs';
+import { join } from 'path';
+import { compileFile as compile, renderFile as render } from 'pug';
+import CCSS from 'clean-css';
 
-const poems = fs.readdirSync('poems').map(fn => path.join('poems', fn)).map(readPoem);
-
-minifyCss();
-writePoems(poems);
-writeIndex(poems);
-
-function readPoem(fileName) {
-  const content = fs.readFileSync(fileName, 'utf-8');
+const readPoem = fileName => {
+  const content = fread(fileName, 'utf-8');
   const [header, text] = splitHeader(content);
   const [title, author] = splitAuthor(header);
-  return { id: header.replace(/(\s|\.|\,)+/g, '-'), title, author, text};
+  return { url: header.replace(/(\s|\.|\,)+/g, '-') + '/', title, author, text };
 }
 
-function splitHeader(s) {
+const splitHeader = s => {
   const i = s.indexOf("\n\n");
   return [s.slice(0, i), s.slice(i + 2)];
 }
 
-function splitAuthor(s) {
+const splitAuthor = s => {
   const i = s.lastIndexOf("\nby");
   return i > 0 ? [s.slice(0, i), s.slice(i + 1)] : [s, undefined];
 }
 
-function writePoems(poems) {
-  const poemToHTML = pug.compileFile('poem.pug');
+const writePoems = poems => {
+  const poemToHTML = compile('poem.pug');
   for (let i = 0, l = poems.length; i < l; i++) {
     const poem = poems[i];
-    poem.prev = poems.at(i - 1).id;
-    poem.next = poems.at((i + 1) % l).id;
-    fs.mkdirSync(poem.id, { recursive: true });
-    fs.writeFileSync(path.join(poem.id, 'index.html'), poemToHTML(poem));
+    poem.prev = poems.at(i - 1).url;
+    poem.next = poems.at((i + 1) % l).url;
+    mkdir(poem.url, { recursive: true });
+    fwrite(join(poem.url, 'index.html'), poemToHTML(poem));
   }
 }
 
-function writeIndex(poems) {
-  fs.writeFileSync('index.html', pug.renderFile('index.pug', { poems }));
-}
 
-function minifyCss() {
-  let styles = fs.readFileSync('./styles.css');
-  fs.writeFileSync('./styles.min.css', (new cleanCSS()).minify(styles).styles);
-}
+const writeIndex = poems => fwrite('index.html', render('index.pug', { poems }));
 
-fs.watch('./', (event, filename) => {
+const minifyCSS = () => fwrite('./styles.min.css', (new CCSS()).minify(fread('./styles.css')).styles);
+
+minifyCSS();
+
+const poems = readdir('poems').map(p => join('poems', p)).map(readPoem);
+writeIndex(poems);
+writePoems(poems);
+console.log('Rebuild');
+
+console.log('Waiting for changes...')
+watch('./', (event, filename) => {
   console.log((new Date).toLocaleTimeString(), event, filename);
   if (event == 'change') {
     try {
       switch (filename) {
         case 'styles.css':
-          minifyCss();
+          minifyCSS();
           writePoems(poems);
           writeIndex(poems);
           break;
@@ -66,7 +64,7 @@ fs.watch('./', (event, filename) => {
           writePoems(poems);
           break;
       }
-    } catch(error) {
+    } catch (error) {
       console.error(error);
     }
   }
